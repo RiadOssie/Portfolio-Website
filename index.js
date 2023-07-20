86b473fb89f2fdda33ef766adc8f45ce7b055678
Riad Oussama <oussamariad2000@gmail.com>
2023-07-20 20:40:57 +0100

    finished all project

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-let svgFirst = document.getElementsByClassName("svg-2")
-let flex = document.getElementsByClassName("flexproject")
-let projects =  document.getElementsByClassName("project")
-function revealProject(){
-   flex.style.maxWidth = "50px";
-   
-}
-projects.onclick = revealProject
+let 
